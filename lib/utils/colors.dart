4835b95c5576399bdd85313b5635989337e11553
Subroto <subroto.2003@gmail.com>
2024-07-18 16:16:26 +0530

    feat: home page UI

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const primaryBg = Colors.white;
 const secondaryColors = Color.fromARGB(255, 6, 130, 120);
+const primaryTextColor = Color.fromARGB(255, 121, 121, 121);
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:celebrare/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,12 +6,54 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Home Page',
-          style: TextStyle(color: Colors.white),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        shadowColor: Colors.black,
+        backgroundColor: primaryBg,
+        leading: const Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+        title: const Text(
+          'Add Image / Icon',
+          style: TextStyle(fontFamily: 'Olivetti', color: primaryTextColor),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: primaryBg,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(5)),
+            child: Column(
+              children: [
+                const Text(
+                  'Upload Image',
+                  style: TextStyle(
+                      fontFamily: 'Olivetti', color: primaryTextColor),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(177, 25),
+                        backgroundColor: secondaryColors,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(3))),
+                    child: const Text(
+                      'Choose from Device',
+                      style: TextStyle(color: Colors.white),
+                    ))
+              ],
+            ),
+          ),
         ),
       ),
     );
